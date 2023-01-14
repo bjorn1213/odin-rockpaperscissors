1dd077b2b9de7b3497a822e629260a8bd400f3d9
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-14 16:09:53 +0100

    Add main function that evaluates a single game

--- main.js
+++ main.js
@@ -15,5 +15,26 @@ function getComputerChoice(){
             choice = 'scissors';        
     }
 
-    return choice
+    return choice;
+}
+
+function playGame(playerSelection, computerSelection=getComputerChoice()){
+    playerSelection = playerSelection.toLowerCase();
+
+    let gameResult = '';
+    let winText = 'You win. The computer chose ' + computerSelection;
+    let drawText = 'The game is a draw. The computer chose ' + computerSelection;
+    let loseText = 'You lose. The computer chose ' + computerSelection;
+
+    if (playerSelection === computerSelection){
+        gameResult = drawText;
+    } else if (playerSelection === 'rock'){
+        gameResult = computerSelection === 'paper' ? loseText : winText;
+    } else if (playerSelection === 'paper'){
+        gameResult = computerSelection === 'scissors' ? loseText : winText;
+    } else { // player selection = scisscors
+        gameResult = computerSelection === 'rock' ? loseText : winText;
+    }
+
+    return gameResult;
 }
