c2d224873a0de8adcc456e93955d88a01cc28ec2
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-01-15 16:11:01 +0100

    Add very basic UI

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 function getComputerChoice(){
     let randomInt = Math.floor(Math.random() * 3);
     let choice = ''
@@ -18,22 +16,22 @@ function getComputerChoice(){
     return choice;
 }
 
-function playRound(playerSelection, computerSelection=getComputerChoice()){
-    playerSelection = playerSelection.toLowerCase();
+function playRound(playerChoice, computerChoice=getComputerChoice()){
+    playerChoice = playerChoice.toLowerCase();
 
     let roundResult = '';
-    let winText = 'You win. The computer chose ' + computerSelection;
-    let drawText = 'The round is a draw. The computer chose ' + computerSelection;
-    let loseText = 'You lose. The computer chose ' + computerSelection;
+    let winText = 'You win. The computer chose ' + computerChoice;
+    let drawText = 'The round is a draw. The computer chose ' + computerChoice;
+    let loseText = 'You lose. The computer chose ' + computerChoice;
 
-    if (playerSelection === computerSelection){
+    if (playerChoice === computerChoice){
         roundResult = drawText;
-    } else if (playerSelection === 'rock'){
-        roundResult = computerSelection === 'paper' ? loseText : winText;
-    } else if (playerSelection === 'paper'){
-        roundResult = computerSelection === 'scissors' ? loseText : winText;
+    } else if (playerChoice === 'rock'){
+        roundResult = computerChoice === 'paper' ? loseText : winText;
+    } else if (playerChoice === 'paper'){
+        roundResult = computerChoice === 'scissors' ? loseText : winText;
     } else { // player selection = scisscors
-        roundResult = computerSelection === 'rock' ? loseText : winText;
+        roundResult = computerChoice === 'rock' ? loseText : winText;
     }
 
     return roundResult;
@@ -57,10 +55,60 @@ function getPlayerChoice(){
     return choice;
 }
 
-function playGame(roundCount=5){
-    let roundResult;
-    for (let round = 0; round < roundCount; round++){
-        roundResult = playRound(getPlayerChoice(), getComputerChoice());
-        console.log(roundResult);
+function getDivByID(divID){
+    let reqDiv;
+    let exists;
+    if (document.querySelector(`#${divID}`)){
+        reqDiv = document.querySelector(`#${divID}`);
+        exists = true;
+    } else {
+        reqDiv = document.createElement('div');
+        reqDiv.id = divID;
+        document.querySelector('body').appendChild(reqDiv);
+        exists = false;
     }
+
+    return reqDiv;
 }
+
+let playerScore = 0;
+let computerScore = 0;
+
+function processOutcome(roundOutcome){
+    if (roundOutcome.includes('win')){
+        playerScore++;
+    } else if (roundOutcome.includes('lose')) {
+        computerScore++;
+    };
+    
+    const scoreDiv = getDivByID('running-score')
+
+    if (playerScore === 5) {
+        scoreDiv.textContent = 'You won 5 times! Click to start over.'
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        scoreDiv.textContent = 'The computer won 5 times! Click to start over.'
+        playerScore = 0;
+        computerScore = 0;
+    } else {
+        scoreDiv.textContent = `You won ${playerScore} times, the computer won ${computerScore} times. First to 5 wins, wins the game.`
+    }
+};
+
+function processBtnPress(playerChoice){
+    const resultDiv = getDivByID('round-result');
+    const roundOutcome = playRound(playerChoice)
+    resultDiv.textContent = roundOutcome;
+
+    processOutcome(roundOutcome);
+}
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const playerChoice = btn.textContent.toLowerCase();
+        processBtnPress(playerChoice);
+    });
+});
